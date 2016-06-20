c2f1fa8049eb1c99847ce6b6354ccea642c618c8
pgris <gris@clermont.in2p3.fr>
2016-06-20 09:55:35 +0200

    Create makeCoaddTempExpConfig.py

---
+++ doc/scripts/makeCoaddTempExp/makeCoaddTempExpConfig.py
@@ -0,0 +1,5 @@
+# overrides for pipe_tasks CoaddTask.ConfigClass
+
+# Default bgSubtracted=False is not working for CFHT / LSST cameras (only for SDSS at the momment)
+config.bgSubtracted=True
+config.doApplyUberCal=True
