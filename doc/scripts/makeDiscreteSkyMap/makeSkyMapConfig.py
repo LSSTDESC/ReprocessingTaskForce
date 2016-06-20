c4a1b2e73fa7b9cbd3bfdf193b797b07a22e176e
pgris <gris@clermont.in2p3.fr>
2016-06-20 09:05:58 +0200

    Add files via upload

---
+++ doc/scripts/makeDiscreteSkyMap/makeSkyMapConfig.py
@@ -0,0 +1,7 @@
+config.skyMap.projection='TAN'
+
+# dimensions of inner region of patches (x,y pixels)
+config.skyMap.patchInnerDimensions=[4000, 4000]
+
+# nominal pixel scale (arcsec/pixel)
+config.skyMap.pixelScale=0.185
