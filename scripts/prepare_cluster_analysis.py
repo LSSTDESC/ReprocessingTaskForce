b65bb872d8583800a46ca61452c5305610133f5e
Nicoals Chotard <nchotard@in2p3.fr>
2017-04-06 17:21:41 +0200

    some work on the script to prepare the reprocessing

--- scripts/prepare_cluster_analysis.py
+++ scripts/prepare_cluster_analysis.py
@@ -56,33 +56,33 @@ build_visit_lists.py -i _parent/input -l _parent/02-processCcd/log
 build_patch_lists.py
 cd _parent
 
-### 03.5-jointcal
+### 04-jointcal
 
-cd 03.5-jointcal
+cd 04-jointcal
 build_visit_lists.py -i _parent/input -l _parent/02-processCcd/log
 jointcal.py _parent/output --output _parent/output/co @r.list --configfile jointcalConfig.py
 same for other filters
 
-### 04-makeCoaddTempExp
+### 05-makeCoaddTempExp
 
-cd 04-makeCoaddTempExp
+cd 05-makeCoaddTempExp
 build_visit_lists.py -i _parent/input/ -l _parent/02-processCcd/log --idopt selectId
 cp _parent/03-makeDiscreteSkyMap/patches* .
 run_makeCoaddTempExp.py -c makeCoaddTempExpConfig.py -a
 cd _parent
 
-### 05-assembleCoadd
+### 06-assembleCoadd
 
-cd 05-assembleCoadd
-cp _parent/04-makeCoaddTempExp/*.list _parent/04-makeCoaddTempExp/*.txt .
+cd 06-assembleCoadd
+cp _parent/05-makeCoaddTempExp/*.list _parent/05-makeCoaddTempExp/*.txt .
 ln -s /sps/lsst/dev/nchotard/clusters/3C295 _parent
 cp /sps/lsst/dev/lsstprod/clusters/MACSJ2243.3-0935/utils/assembleCoadd/assembleConfig.py .
 run_assembleCoadd.py -c assembleConfig.py
 
 
-### 06-detectCoaddSources
+### 07-detectCoaddSources
 
-cp ../04-makeCoaddTempExp/*.txt .
+cp ../05-makeCoaddTempExp/*.txt .
 ln -s /sps/lsst/dev/nchotard/clusters/3C295 _parent
 cp /sps/lsst/dev/lsstprod/clusters/MACSJ2243.3-0935/utils/detectCoaddSources/detectCoaddConfig.py .
 run_detectCoaddSources.py -c detectCoaddConfig.py -a
@@ -134,14 +134,15 @@ DIRS = ['00-CalibratedData',
         '01-AstrometryData',
         '02-processCcd',
         '03-makeDiscreteSkyMap',
-        '04-makeCoaddTempExp',
-        '05-assembleCoadd',
-        '06-detectCoaddSources',
-        '07-mergeCoaddDetections',
-        '08-measureCoaddSources',
-        '09-mergeCoaddMeasurements',
-        '10-forcedPhotCcd',
-        '11-forcedPhotCoadd',
+        '04-jointcal',
+        '05-makeCoaddTempExp',
+        '06-assembleCoadd',
+        '07-detectCoaddSources',
+        '08-mergeCoaddDetections',
+        '09-measureCoaddSources',
+        '10-mergeCoaddMeasurements',
+        '11-forcedPhotCcd',
+        '12-forcedPhotCoadd',
         'output',
         'input']
 FILES = ['setup.sh',
@@ -150,14 +151,15 @@ CONFIGS = ['01-andConfig.py',
            '02-processConfig.py',
            '02-processConfig_u.py',
            '03-makeSkyMapConfig.py',
-           '04-makeCoaddTempExpConfig.py',
-           '05-assembleConfig.py',
-           '06-detectCoaddConfig.py',
-           '07-mergeCoaddDetections.py',
-           '08-measureCoaddSources.py',
-           '09-mergeCoaddMeasurements.py',
-           '10-forcedPhotCcd.py',
-           '11-forcedPhotCoadd.py']
+           '04-jointcalConfig.py',
+           '05-makeCoaddTempExpConfig.py',
+           '06-assembleConfig.py',
+           '07-detectCoaddConfig.py',
+           '08-mergeCoaddDetections.py',
+           '09-measureCoaddSources.py',
+           '10-mergeCoaddMeasurements.py',
+           '11-forcedPhotCcd.py',
+           '12-forcedPhotCoadd.py']
 
 # Where the config file templates are stored
 CTEMPLATES = "/sps/lsst/dev/nchotard/stack/configs/"
@@ -178,7 +180,10 @@ if __name__ == "__main__":
     opts, args = parser.parse_args()
 
     # Create the directories
-    current_dir = os.path.abspath('./')
+    if not os.path.isdir(opts.target):
+        os.mkdir(opts.target)
+    os.chdir(opts.target)
+    current_dir = os.path.abspath('./') + '/'
     for d in DIRS:
         if opts.datadir is not None and d in ['00-CalibratedData', 'output', 'input']:
             if not opts.datadir.endswith('/'):
@@ -189,14 +194,17 @@ if __name__ == "__main__":
         else:
             if not os.path.isdir(d):
                 os.mkdir(d)
-        if not os.path.exists(d+'/_parent'):
-            os.symlink(current_dir, d+'/_parent')
+        if not os.path.exists(d + '/_parent'):
+            os.symlink(current_dir, d + '/_parent')
+        if not d.startswith(('00', '01')):
+            os.mkdir(current_dir + d + '/log')
+            os.mkdir(current_dir + d + '/scripts')
 
     # Declare an instrument mapper for the DM butler
     mapper = open("input/_mapper", 'w')
     mapper.write("lsst.obs.cfht.MegacamMapper")
     mapper.close()
-    
+
     # Copy the config files
     for c in CONFIGS:
         os.system("cp -v %s %s-*/" % (CTEMPLATES+c.split('-')[1], c.split('-')[0]))
@@ -205,7 +213,7 @@ if __name__ == "__main__":
     setup = open("setup.sh", 'w')
     setup.write(SETUP)
     setup.close()
-    
+
     # Create the README
     readme = open("README", 'w')
     readme.write(README % (opts.target))
