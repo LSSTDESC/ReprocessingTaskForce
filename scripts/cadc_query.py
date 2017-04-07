34a4329129367e5b981fac0a6fb76001a0eed59e
Nicoals Chotard <nchotard@in2p3.fr>
2017-04-07 15:58:44 +0200

    type

--- scripts/cadc_query.py
+++ scripts/cadc_query.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     parser.add_option('-o', '--output', type='string', default='cadcUrlList.txt',
                       help="Name of the output file which will contain the list"
                       " of fits to download [%default]")
-    parser.add_option('-d', '--dir', type='string', defaul=None,
+    parser.add_option('-d', '--dir', type='string', default=None,
                       help="Directory where data will be downloaded. Default is target name."
     parser.add_option('-D', '--download', action='store_true', default=False,
                       help="Automatic download of the fits files found (could take a while...)")
