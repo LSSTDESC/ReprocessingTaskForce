5ef4adbb6179dbca3b3656f3229159fb79e01ff9
Nicoals Chotard <nchotard@in2p3.fr>
2017-04-10 10:59:54 +0200

    download files in local directory

--- scripts/reprocessing_setup.py
+++ scripts/reprocessing_setup.py
@@ -27,8 +27,8 @@ source setup.sh
 ### Get the data: 00-CalibratedData
 
 cd 00-CalibratedData
-cadc_query.py -T %s             # to check the available data  
-cadc_query.py -T %s -d "./" -D  # to download them
+cadc_query.py -T %s -d .     # to check the available data  
+cadc_query.py -T %s -d . -D  # to download them
 cd _parent
 
 ### Re-organize the data
