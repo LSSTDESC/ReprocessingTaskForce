bff1b428037a0a165725997a0333d53010aa621d
Nicoals Chotard <nchotard@in2p3.fr>
2017-04-19 16:01:09 +0200

    change the os

--- scripts/run_makeCoaddTempExp.py
+++ scripts/run_makeCoaddTempExp.py
@@ -20,12 +20,12 @@ if __name__ == "__main__":
     and launch processCcd in several batch jobs. You thus need to be running it at CC-IN2P3 to make 
     it work. To run all  filters, you can do something like 
     %prog -f ugriz -m 1 -c makeCoaddTempExpConfig.py -a"""
-    
+
     opts, args = LR.standard_options(usage=usage, description=description, filters=filters)
 
     # overwrite the -m option to force it to be 1
     opts.mod = 1
-    
+
     # Loop over filters
     for filt in opts.filters:
 
@@ -37,21 +37,21 @@ if __name__ == "__main__":
 
         # How many jobs should we be running (and how many visit in each?)?
         njobs = LR.job_number(patches, opts.mod, opts.max)
-        
+
         # Reorganize the visit list in consequence
         patches = LR.organize_items(patches, njobs)
 
         # Loop over the patches sub lists
         for ps in patches:
-            
+
             # Build the command line and other things
             cmd = build_cmd(ps[0], config, filt)
-            
+
             # Only submit the job if asked
             prefix = LR.makeFileName(ps[0])
             LR.submit(cmd, prefix, filt, autosubmit=opts.autosubmit,
-                      ct=1000)
-        
+                      ct=1000, queue='long', system=opts.system)
+
     if not opts.autosubmit:
         print "\nINFO: Use option --autosubmit to submit the jobs"
 
