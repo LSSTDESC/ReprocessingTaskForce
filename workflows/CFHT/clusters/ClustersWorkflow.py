5433fab415b81d3445731ac9e5fd5b65991ceb0c
Nicoals Chotard <nchotard@in2p3.fr>
2017-04-25 16:52:02 +0200

    Adapt scripts to work when job are launched from slac

--- workflows/CFHT/clusters/ClustersWorkflow.py
+++ workflows/CFHT/clusters/ClustersWorkflow.py
@@ -4,6 +4,10 @@ def runscripts():
     import glob, os
     workdir = os.getenv("WORK_DIR")[0] + "/02-processccd/"
     scripts = glob.glob(workdir + "scripts/*/*.sh")
+    if not len(scripts):
+        raise("ERROR: no file found")
+    else:
+        raise("GREAT: %i file found" % len(scripts))
     for i, script in enumerate(scripts):
         print "%2d" % i, script
         vars = MashMap()
