b214b33dda0e768030e65bbead8b2886a2774efc
Nicoals Chotard <nchotard@in2p3.fr>
2017-04-26 09:59:43 +0200

    Adapt scripts to work when job are launched from slac

--- workflows/CFHT/clusters/ClustersWorkflow.py
+++ workflows/CFHT/clusters/ClustersWorkflow.py
@@ -2,13 +2,13 @@ from java.util import HashMap
 
 def runscripts():
     import glob, os
-    stream = os.getenv("PIPELINE_STREAMPATH")[0]
-    WORK_DIR = OUTPUT_DATA_DIR + "/work/" + str(stream)
-    scripts = glob.glob(WORK_DIR + "/02-processccd/scripts/*/*.sh")
+    process = pipeline.getProcessInstance("setup_processccd")
+    vars = HashMap(process.getVariables())
+    scripts = glob.glob(vars["WORK_DIR"] + "/02-processccd/scripts/*/*.sh")
     if not len(scripts):
-        raise("ERROR: no file found")
+        raise "ERROR: no file found"
     else:
-        raise("GREAT: %i file found" % len(scripts))
+        raise "GREAT: %i file found" % len(scripts)
     for i, script in enumerate(scripts):
         print "%2d" % i, script
         vars = HashMap()
