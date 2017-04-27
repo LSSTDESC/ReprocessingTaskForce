f49d8215b52248fb2223101e6444edfa25b18b7a
Nicoals Chotard <nchotard@in2p3.fr>
2017-04-27 12:31:25 +0200

    Adapt scripts to work when job are launched from slac

--- workflows/CFHT/clusters/ClustersWorkflow.py
+++ workflows/CFHT/clusters/ClustersWorkflow.py
@@ -3,9 +3,12 @@ from java.util import HashMap
 def runscripts():
     process = pipeline.getProcessInstance("setup_processccd")
     vars = HashMap(process.getVariables())
+    workdir = vars.remove("WORK_DIR")
+    num = 0
     for filt in ['u', 'g', 'r', 'i', 'z']:
-        nscript = vars['n' + filt + 'scripts']
+        nscript = vars.remove('n' + filt + 'scripts')
         for i in range(1, int(nscript) + 1):
-            script = vars["WORK_DIR"] + "/02-processccd/scripts/%s/visit_%03d_script.sh" % (filt, i)
+            script = workdir + "/02-processccd/scripts/%s/visit_%03d_script.sh" % (filt, i)
             vars.put("CUR_SCRIPT", script)
-            pipeline.createSubstream("processFilter", i, vars)
+            pipeline.createSubstream("processFilter", num, vars)
+            num += 1
