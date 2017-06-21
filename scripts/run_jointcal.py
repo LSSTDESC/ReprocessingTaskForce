cac5427b4f89e2c4798c0480931e6710691bf728
Nicoals Chotard <nchotard@in2p3.fr>
2017-06-21 14:39:22 +0200

    change output direcoties

--- scripts/run_jointcal.py
+++ scripts/run_jointcal.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     opts, args = LR.standard_options(usage=usage, description=description, filters=filters)
 
-    input = "pardir/output/coadd_dir"
+    input = "pardir/output"
     output = "pardir/output/jointcal"
     config = "jointcalConfig.py"
 
