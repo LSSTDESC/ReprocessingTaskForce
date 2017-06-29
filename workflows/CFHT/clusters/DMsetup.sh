7e210b97b73b799ff9c765c1234bf2114d6b880d
Nicoals Chotard <nchotard@in2p3.fr>
2017-06-29 11:44:35 +0200

    Try older version of the stack

--- workflows/CFHT/clusters/DMsetup.sh
+++ workflows/CFHT/clusters/DMsetup.sh
@@ -2,10 +2,12 @@
 export HOME=`pwd`
 
 # Setup for the stack
-source ${DM_DIR}/${DM_SETUP}
-setup lsst_distrib
-setup obs_cfht
-setup pipe_tasks
-setup galsim
-setup meas_extensions_psfex
-setup meas_modelfit
+source ${DM_SETUP}
+
+#source ${DM_DIR}/${DM_SETUP}
+#setup lsst_distrib
+#setup obs_cfht
+#setup pipe_tasks
+#setup galsim
+#setup meas_extensions_psfex
+#setup meas_modelfit
