4eb4a2112486136867623b115b7495f85dc219db
Nicoals Chotard <nchotard@in2p3.fr>
2017-07-04 17:05:51 +0200

    Addapt the scripts for different output + add DM workflow for weeklies

--- scripts/run_mergeCoaddMeasurements.py
+++ scripts/run_mergeCoaddMeasurements.py
@@ -30,8 +30,8 @@ if __name__ == "__main__":
     opts, args = LR.standard_options(usage=usage, description=description)
 
     opts.mod = 10
-    opts.input = "pardir/output"
-    opts.output = "pardir/output"
+    opts.input = "pardir/output/measureCoaddSources"
+    opts.output = "pardir/output/mergecoaddmeasurements"
     file_patch = "patches_all.txt"
     
     cmd = "split -l " + str(opts.mod) + " -d " + file_patch + " " + file_patch + "_"
