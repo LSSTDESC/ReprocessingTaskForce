6455fc0ca493c20c20f972d54459a96e5874f848
Nicoals Chotard <nchotard@in2p3.fr>
2017-08-01 18:45:49 +0200

    Try w_2017_30

---
+++ config/w_2017_30/jointcalConfig.py
@@ -0,0 +1,42 @@
+# Configuration file for jointcal
+
+from lsst.meas.algorithms import LoadIndexedReferenceObjectsTask
+
+# Select external catalogs for Astrometry
+config.astrometryRefObjLoader.retarget(LoadIndexedReferenceObjectsTask)
+config.astrometryRefObjLoader.ref_dataset_name='pan-starrs'
+config.astrometryRefObjLoader.filterMap = {
+    'u':'g',
+    'g':'g',
+    'r':'r',
+    'i':'i',
+    'i2': 'i',
+    'z':'z',
+    'y':'y',
+}
+
+# Select external catalogs for Photometry
+config.doPhotometry = True  # comment out to run the photometric calibration
+config.photometryRefObjLoader.retarget(LoadIndexedReferenceObjectsTask)
+config.photometryRefObjLoader.ref_dataset_name='sdss'
+config.photometryRefObjLoader.filterMap = {
+    'u': 'U',
+    'g': 'G',
+    'r': 'R',
+    'i': 'I',
+    'i2': 'I',
+    'z': 'Z',
+    'y': 'Z',
+}
+
+# These are the default values
+
+# Minimum allowed signal-to-noise ratio for sources used for matching
+# (in the flux specified by sourceFluxType); <= 0 for no limit
+# config.sourceSelector['matcher'].minSnr = 40.0
+
+# Minimum allowed signal-to-noise ratio for sources used for matching
+# (in the flux specified by sourceFluxType); <= 0 for no limit
+# config.sourceSelector['astrometry'].minSnr = 10.0
+
+
---
+++ config/w_2017_30/singleFrameDriverConfig.py
@@ -0,0 +1,96 @@
+# Apply the brighter fatter correction
+config.processCcd.isr.doBrighterFatter=False
+
+config.processCcd.charImage.repair.cosmicray.nCrPixelMax=1000000
+
+# Useful to get to avoid deblending of satellite tracks
+config.processCcd.calibrate.deblend.maxFootprintSize=2000 #2200
+
+# Use psfex instead of pca
+import lsst.meas.extensions.psfex.psfexPsfDeterminer
+config.processCcd.charImage.measurePsf.psfDeterminer.name='psfex'
+
+# The following should be included for u filter in order to lower the source detection threshold
+#config.processCcd.charImage.detection.includeThresholdMultiplier=1.0
+
+# Run CModel
+import lsst.meas.modelfit
+config.processCcd.charImage.measurement.plugins.names |= ["modelfit_DoubleShapeletPsfApprox", "modelfit_CModel"]
+
+# Run astrometry using the new htm reference catalog format
+# The following retargets are necessary until the new scheme becomes standard
+from lsst.meas.algorithms import LoadIndexedReferenceObjectsTask
+config.processCcd.calibrate.astromRefObjLoader.retarget(LoadIndexedReferenceObjectsTask)
+config.processCcd.calibrate.photoRefObjLoader.retarget(LoadIndexedReferenceObjectsTask)
+
+# Use new astrometry fitter
+from lsst.meas.astrom import FitSipDistortionTask
+config.processCcd.calibrate.astrometry.wcsFitter.retarget(FitSipDistortionTask)
+
+config.processCcd.calibrate.astrometry.wcsFitter.order = 3
+config.processCcd.calibrate.astrometry.matcher.maxMatchDistArcSec=5
+
+# Select external catalogs for Astrometry and Photometry
+config.processCcd.calibrate.photoRefObjLoader.ref_dataset_name='sdss'
+#config.processCcd.calibrate.astromRefObjLoader.ref_dataset_name='gaia'
+config.processCcd.calibrate.astromRefObjLoader.ref_dataset_name='pan-starrs'
+#config.processCcd.calibrate.astromRefObjLoader.ref_dataset_name='sdss'
+
+# Astrometry with panstarrs
+config.processCcd.calibrate.astromRefObjLoader.filterMap = {
+    'u':'g',
+    'g':'g',
+    'r':'r',
+    'i':'i',
+    'i2': 'i',
+    'z':'z',
+    'y':'y',
+}
+# Astrometry with gaia
+#config.processCcd.calibrate.astromRefObjLoader.filterMap = {
+#    'u':'phot_g_mean_mag',
+#    'g':'phot_g_mean_mag',
+#    'r':'phot_g_mean_mag',
+#    'i':'phot_g_mean_mag',
+#    'z':'phot_g_mean_mag',
+#    'y':'phot_g_mean_mag',
+#}
+# Photometry with sdss
+config.processCcd.calibrate.photoRefObjLoader.filterMap = {
+    'u': 'U',
+    'g': 'G',
+    'r': 'R',
+    'i': 'I',
+    'i2': 'I',
+    'z': 'Z',
+    'y': 'Z',
+}
+
+#Astrometry with sdss
+#config.processCcd.calibrate.astromRefObjLoader.filterMap = {
+#    'u': 'U',
+#    'g': 'G',
+#    'r': 'R',
+#    'i': 'I',
+#    'z': 'Z',
+#    'y': 'Z',
+#}
+
+import lsst.pipe.tasks.colorterms
+config.processCcd.calibrate.photoCal.colorterms.data['e2v'].data['i2']=lsst.pipe.tasks.colorterms.Colorterm()
+config.processCcd.calibrate.photoCal.colorterms.data['e2v'].data['i2'].c2=0.0
+config.processCcd.calibrate.photoCal.colorterms.data['e2v'].data['i2'].c1=0.003
+config.processCcd.calibrate.photoCal.colorterms.data['e2v'].data['i2'].c0=0.0
+config.processCcd.calibrate.photoCal.colorterms.data['e2v'].data['i2'].primary='i'
+config.processCcd.calibrate.photoCal.colorterms.data['e2v'].data['i2'].secondary='r'
+
+# use Chebyshev background estimation
+config.processCcd.charImage.background.useApprox=True
+config.processCcd.charImage.detection.background.binSize=128
+config.processCcd.charImage.detection.background.useApprox=True
+config.processCcd.charImage.background.binSize = 128
+config.processCcd.charImage.background.undersampleStyle = 'REDUCE_INTERP_ORDER'
+config.processCcd.charImage.detection.background.binSize = 128
+config.processCcd.charImage.detection.background.undersampleStyle='REDUCE_INTERP_ORDER'
+config.processCcd.charImage.detection.background.binSize = 128
+config.processCcd.charImage.detection.background.undersampleStyle = 'REDUCE_INTERP_ORDER'
