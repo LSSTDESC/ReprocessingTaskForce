ef1479daa6aaf89c3ae6f57ee4dd3261e336231e
Nicolas Chotard <nchotard@in2p3.fr>
2017-12-12 17:01:15 +0100

    Update assembleCoaddConfig.py

--- config/w_2017_49/assembleCoaddConfig.py
+++ config/w_2017_49/assembleCoaddConfig.py
@@ -1,7 +1,7 @@
 #I am not sure about the following line <== To be checked
 config.badMaskPlanes = ("BAD", "EDGE", "SAT", "INTRP", "NO_DATA",)
 
-config.doMatchBackgrounds = False
+# config.doMatchBackgrounds = False
 
 # Mask planes to remove before coadding
 config.removeMaskPlanes=['NOT_DEBLENDED']  # ['CROSSTALK', 'NOT_DEBLENDED']
