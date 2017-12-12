2bc6e77394839b135e90c30d219cee7215970206
Nicolas Chotard <nchotard@in2p3.fr>
2017-12-12 21:08:40 +0100

    Update forcedPhotCoaddConfig.py

--- config/w_2017_49/forcedPhotCoaddConfig.py
+++ config/w_2017_49/forcedPhotCoaddConfig.py
@@ -6,7 +6,7 @@ config.measurement.slots.modelFlux = "modelfit_CModel"
 
 config.doApCorr=True
 
-config.measurement.plugins['base_PixelFlags'].masksFpAnywhere.append('CLIPPED')
+#config.measurement.plugins['base_PixelFlags'].masksFpAnywhere.append('CLIPPED')
 # These do not work anymore??
 #config.measurement.plugins['base_PixelFlags'].masksFpCenter.append('BRIGHT_OBJECT')
 #config.measurement.plugins['base_PixelFlags'].masksFpAnywhere.append('BRIGHT_OBJECT')
