ce0a493d6c127fbef77cb20082aa92d3f7f8b5b0
Nicoals Chotard <nchotard@in2p3.fr>
2018-01-12 07:43:16 +0100

    Setup the right obs_cfht package

--- workflows/CFHT/clusters/DMsetup.sh
+++ workflows/CFHT/clusters/DMsetup.sh
@@ -7,10 +7,12 @@ source ${DM_SETUP}
 if [[ $DM_SETUP == *"/sps/lsst/software/lsst_distrib/"* ]]
 then
     setup lsst_distrib
-    setup obs_cfht -t tractbugfix
     setup pipe_tasks
     setup galsim
     setup meas_extensions_psfex
     setup meas_modelfit
     setup pipe_drivers
+    cd /sps/lsst/users/nchotard/obs_cfht
+    eups declare -r . obs_cfht tractbugfix
+    setup obs_cfht -t tractbugfix
 fi
